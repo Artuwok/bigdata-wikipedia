50010e06591c7a63ac7b875486191823de4f1e6d
Artuwok1986 <artem.vlasenko@gmail.com>
2016-07-21 14:16:44 +0300

    Initial first commit. - added and partly configured maven pom.xml - added git ignore file - basic Spring app config made

---
+++ rialto/src/main/java/com/rialto/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.rialto.configuration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.rialto")
+public class AppConfig {
+}
